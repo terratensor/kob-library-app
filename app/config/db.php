8a0b288300fb0a649d4a735de04f8394f78602c3
agusev <audetv@gmail.com>
2023-06-26 12:28:17 +0300

    Configured db postgres

--- app/config/db.php
+++ app/config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'class' => \yii\db\Connection::class,
+    'dsn' => 'pgsql:host='.getenv('POSTGRES_HOST').';dbname=' . getenv('POSTGRES_DB'),
+    'username' => getenv('POSTGRES_USER'),
+    'password' => trim(file_get_contents(getenv('POSTGRES_PASSWORD_FILE'))),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
