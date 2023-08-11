2d40673ddd20d5b8b3ff0bedb750c4c09b54201a
agusev <audetv@gmail.com>
2023-08-12 01:12:38 +0300

    Added meta tags

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -15,7 +15,6 @@ $this->registerCsrfMetaTags();
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
 $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
-$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
